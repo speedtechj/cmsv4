e9bd7f6f31e1eaf0a16451ce12d9c24e1d4e1b15
speedtechj <arbiasjr@gmail.com>
2024-03-30 15:54:58 -0600

    update

--- app/Filament/Exports/CollectionreportExporter.php
+++ app/Filament/Exports/CollectionreportExporter.php
@@ -2,14 +2,14 @@
 
 namespace App\Filament\Exports;
 
-use App\Models\Booking;
+use App\Models\Collectionreport;
 use Filament\Actions\Exports\ExportColumn;
 use Filament\Actions\Exports\Exporter;
 use Filament\Actions\Exports\Models\Export;
 
-class BookingExporter extends Exporter
+class CollectionreportExporter extends Exporter
 {
-    protected static ?string $model = Booking::class;
+    protected static ?string $model = Collectionreport::class;
 
     public static function getColumns(): array
     {
@@ -19,11 +19,6 @@ class BookingExporter extends Exporter
             ExportColumn::make('sender.full_name')->label('Sender'),
             ExportColumn::make('senderaddress.address')->label('Sender Address'),
             ExportColumn::make('senderaddress.quadrant')->label('Quadrant'),
-            ExportColumn::make('senderaddress.provincecan.name')->label('Province'),
-            ExportColumn::make('senderaddress.citycan.name')->label('City/Municipality'),
-            ExportColumn::make('senderaddress.postal_code')->label('Postal Code'),
-            ExportColumn::make('sender.mobile_no')->label('Mobile No'),
-            ExportColumn::make('sender.home_no')->label('Home No'),
             ExportColumn::make('zone.description')->label('Zone'),
             ExportColumn::make('boxtype.description')->label('Box Type'),
             ExportColumn::make('booking_date')->label('Booking Date'),
@@ -37,13 +32,12 @@ class BookingExporter extends Exporter
             ExportColumn::make('discount.discount_amount'),
             ExportColumn::make('total_price'),
             ExportColumn::make('payment_balance'),
-           
         ];
     }
 
     public static function getCompletedNotificationBody(Export $export): string
     {
-        $body = 'Your booking export has completed and ' . number_format($export->successful_rows) . ' ' . str('row')->plural($export->successful_rows) . ' exported.';
+        $body = 'Your collectionreport export has completed and ' . number_format($export->successful_rows) . ' ' . str('row')->plural($export->successful_rows) . ' exported.';
 
         if ($failedRowsCount = $export->getFailedRowsCount()) {
             $body .= ' ' . number_format($failedRowsCount) . ' ' . str('row')->plural($failedRowsCount) . ' failed to export.';
