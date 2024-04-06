e421fa9aa94b2b0740077977d8546a9327044394
speedtechj <arbiasjr@gmail.com>
2024-04-06 13:40:12 -0600

    update

--- app/Filament/Appuser/Resources/SenderResource.php
+++ app/Filament/Appuser/Resources/SenderResource.php
@@ -104,7 +104,7 @@ class SenderResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                // Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
