6881d22943c0a494778bfce11b804aa0fa69b9ee
speedtechj <arbiasjr@gmail.com>
2024-05-01 09:57:16 -0600

    update

--- app/Filament/Twelve24/Resources/SearchinvoiceResource/Pages/ListSearchinvoices.php
+++ app/Filament/Twelve24/Resources/SearchinvoiceResource/Pages/ListSearchinvoices.php
@@ -5,7 +5,8 @@ namespace App\Filament\Twelve24\Resources\SearchinvoiceResource\Pages;
 use App\Filament\Twelve24\Resources\SearchinvoiceResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
-
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Contracts\Pagination\Paginator;
 class ListSearchinvoices extends ListRecords
 {
     protected static string $resource = SearchinvoiceResource::class;
@@ -16,4 +17,8 @@ class ListSearchinvoices extends ListRecords
             // Actions\CreateAction::make(),
         ];
     }
+    protected function paginateTableQuery(Builder $query): Paginator
+{
+    return $query->simplePaginate(($this->getTableRecordsPerPage() === 'all') ? $query->count() : $this->getTableRecordsPerPage());
+}
 }
