0d8bd7734e02fff7c10759af4d3516232502fc40
speedtechj <arbiasjr@gmail.com>
2024-05-16 09:29:09 -0600

    update

--- app/Filament/Appuser/Resources/ManifestResource/Pages/ListManifests.php
+++ app/Filament/Appuser/Resources/ManifestResource/Pages/ListManifests.php
@@ -5,7 +5,8 @@ namespace App\Filament\Appuser\Resources\ManifestResource\Pages;
 use App\Filament\Appuser\Resources\ManifestResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
-
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Builder;
 class ListManifests extends ListRecords
 {
     protected static string $resource = ManifestResource::class;
@@ -16,4 +17,8 @@ class ListManifests extends ListRecords
             // Actions\CreateAction::make(),
         ];
     }
+    protected function paginateTableQuery(Builder $query): Paginator
+{
+    return $query->simplePaginate(($this->getTableRecordsPerPage() === 'all') ? $query->count() : $this->getTableRecordsPerPage());
+}
 }
